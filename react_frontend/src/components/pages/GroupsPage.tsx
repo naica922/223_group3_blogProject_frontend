d26c7c6f6409006576f263bda9b64c2a01d09711
ncavegn <143715125+ncavegn@users.noreply.github.com>
2025-02-21 15:54:25 +0100

    Change route and add button to create form

--- react_frontend/src/components/pages/GroupsPage.tsx
+++ react_frontend/src/components/pages/GroupsPage.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import CreateGroupFormPage from "./CreateGroupFormPage";
+import { useNavigate } from 'react-router-dom';
+import { Button } from "@mui/material";
 
 export default function GroupsPage() {
+    const navigate = useNavigate();
+
+    const handleNavigateToCreateGroup = () => {
+        navigate('/create');
+    };
+
     return (
-        <>
+        <div>
             <h1>Groups</h1>
-            <CreateGroupFormPage></CreateGroupFormPage></>
+            <Button variant="contained" onClick={handleNavigateToCreateGroup}>
+                Create Group
+            </Button>
+        </div>
     );
 }
